4cb2840d975f8ca32b6c46b2e21ba89f254a6c69
cmmadnat <cmmadnat@gmail.com>
2017-10-12 19:47:16 +0700

    add kotlin and sample projects

---
+++ src/main/java/demo/Application.kt
@@ -0,0 +1,33 @@
+package demo
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@RestController
+class Application {
+
+    @Value("\${info.version}")
+    @get:RequestMapping("/version")
+    val version: String? = null
+
+    @RequestMapping("/")
+    fun home(): String {
+        return "Hello World"
+    }
+
+    companion object {
+
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, args)
+        }
+    }
+
+}

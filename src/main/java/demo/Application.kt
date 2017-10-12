eec7104e7eebc5a618914967360f06e3ac2bcc64
cmmadnat <cmmadnat@gmail.com>
2017-10-12 20:40:18 +0700

    add hn service things

--- src/main/java/demo/Application.kt
+++ src/main/java/demo/Application.kt
@@ -2,11 +2,13 @@ package demo
 
 import demo.service.HnService
 import demo.service.HnServiceImpl
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Configuration
@@ -18,11 +20,15 @@ open class Application {
 
 @Controller
 @RequestMapping("/")
-class HomeController() {
+class HomeController {
     @RequestMapping
-    fun index(): String {
+    fun index(model: Model): String {
+        val list = hnService.findAll(0, 200)
+        model.addAttribute("list", list)
         return "hnList"
     }
+
+    @Autowired lateinit var hnService: HnService
 }
 
 fun main(args: Array<String>) {
--- src/main/java/demo/service/HnService.kt
+++ src/main/java/demo/service/HnService.kt
@@ -1,19 +1,27 @@
 package demo.service
 
 import com.googlecode.objectify.ObjectifyService
+import com.googlecode.objectify.ObjectifyService.ofy
 import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
 import com.googlecode.objectify.annotation.Index
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
+import java.util.*
 
-@Entity data class HospitalNumber(@Id var id: Long? = null, var name: String = "", @Index var hn: String = "")
+@Entity data class HospitalNumber(@Id var id: Long? = null, var name: String = "", @Index var hn: String = "",
+                                  @Index var date: Date = Date())
 
 interface HnService {
+    fun findAll(page: Int, limit: Int): MutableList<HospitalNumber>
 }
 
 @Service
 class HnServiceImpl : HnService, CommandLineRunner {
+    override fun findAll(page: Int, limit: Int): MutableList<HospitalNumber> {
+        return ofy().load().type(HospitalNumber::class.java).limit(limit).order("-date").offset(page * limit).list()
+    }
+
     override fun run(vararg p0: String?) {
         ObjectifyService.register(HospitalNumber::class.java)
     }

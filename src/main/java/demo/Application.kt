92d378286b6e78e1caa7f2233f87fc0f10c1e3a6
cmmadnat <cmmadnat@gmail.com>
2017-10-12 21:13:03 +0700

    add text field, error

--- src/main/java/demo/Application.kt
+++ src/main/java/demo/Application.kt
@@ -2,6 +2,7 @@ package demo
 
 import demo.service.HnService
 import demo.service.HnServiceImpl
+import demo.service.HospitalNumber
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
@@ -9,7 +10,16 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.Errors
+import org.springframework.validation.ValidationUtils
+import org.springframework.validation.Validator
+import org.springframework.web.bind.WebDataBinder
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.InitBinder
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import javax.validation.Valid
 
 @Configuration
 @EnableAutoConfiguration
@@ -21,11 +31,36 @@ open class Application {
 @Controller
 @RequestMapping("/")
 class HomeController {
-    @RequestMapping
-    fun index(model: Model): String {
+    @GetMapping
+    fun index(model: Model, hospitalNumber: HospitalNumber): String {
         val list = hnService.findAll(0, 200)
         model.addAttribute("list", list)
         return "hnList"
+
+    }
+    @InitBinder("hospitalNumber")
+    fun hospitalNumber(webDataBinder: WebDataBinder){
+        webDataBinder.addValidators(HospitalNumberValidator())
+    }
+
+    class HospitalNumberValidator : Validator {
+        override fun validate(p0: Any?, p1: Errors?) {
+            ValidationUtils.rejectIfEmpty(p1, "hn", "required")
+        }
+
+        override fun supports(p0: Class<*>): Boolean {
+            return p0.isAssignableFrom(HospitalNumber::class.java)
+        }
+
+    }
+
+    @PostMapping("new")
+    fun newHospitalNumber(@Valid hospitalNumber: HospitalNumber, bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors()) {
+            return "hnListNew"
+        }
+        hnService.newHospitalNumber(hospitalNumber)
+        return "redirect:/"
     }
 
     @Autowired lateinit var hnService: HnService
--- src/main/java/demo/service/HnService.kt
+++ src/main/java/demo/service/HnService.kt
@@ -10,14 +10,19 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Entity data class HospitalNumber(@Id var id: Long? = null, var name: String = "", @Index var hn: String = "",
-                                  @Index var date: Date = Date())
+                                  @Index var date: Date = Date(), @Index var appUser:Long?=null)
 
 interface HnService {
     fun findAll(page: Int, limit: Int): MutableList<HospitalNumber>
+    fun newHospitalNumber(hospitalNumber: HospitalNumber): Long
 }
 
 @Service
 class HnServiceImpl : HnService, CommandLineRunner {
+    override fun newHospitalNumber(hospitalNumber: HospitalNumber): Long {
+        return ofy().save().entity(hospitalNumber).now().id
+    }
+
     override fun findAll(page: Int, limit: Int): MutableList<HospitalNumber> {
         return ofy().load().type(HospitalNumber::class.java).limit(limit).order("-date").offset(page * limit).list()
     }

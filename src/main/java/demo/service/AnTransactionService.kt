2c4843c8b9c2739454e9f19dc52bf1b913558458
cmmadnat <cmmadnat@gmail.com>
2017-10-13 17:33:40 +0700

    fix admission transaction registration

--- src/main/java/demo/service/AnTransactionService.kt
+++ src/main/java/demo/service/AnTransactionService.kt
@@ -1,9 +1,11 @@
 package demo.service
 
+import com.googlecode.objectify.ObjectifyService
 import com.googlecode.objectify.ObjectifyService.ofy
 import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
 import com.googlecode.objectify.annotation.Index
+import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -14,7 +16,11 @@ interface AnTransactionService {
 }
 
 @Service
-class AnTransactionServiceImpl : AnTransactionService {
+class AnTransactionServiceImpl : AnTransactionService, CommandLineRunner {
+    override fun run(vararg p0: String?) {
+        ObjectifyService.register(AnTransaction::class.java)
+    }
+
     override fun findByAn(an: String): List<AnTransaction> {
         return ofy().load().type(AnTransaction::class.java).filter("an", an).order("date").list()
     }

47aed66aaa7e3a0d3259c9b36b29e438c01e0e70
cmmadnat <cmmadnat@gmail.com>
2017-10-13 17:19:06 +0700

    add an transaction controller

--- src/main/java/demo/service/AnTransactionService.kt
+++ src/main/java/demo/service/AnTransactionService.kt
@@ -1,12 +1,22 @@
 package demo.service
 
+import com.googlecode.objectify.ObjectifyService.ofy
 import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
+import com.googlecode.objectify.annotation.Index
+import org.springframework.stereotype.Service
+import java.util.*
 
-@Entity data class AnTransaction(@Id var id: Long? = null, var detail: String)
+@Entity data class AnTransaction(@Id var id: Long? = null, @Index var an: String = "", var detail: String, var date: Date = Date())
 
-interface AnTransactionService
+interface AnTransactionService {
+    fun findByAn(an: String): List<AnTransaction>
+}
+
 @Service
-class AnTransactionServiceImpl:AnTransactionService{
+class AnTransactionServiceImpl : AnTransactionService {
+    override fun findByAn(an: String): List<AnTransaction> {
+        return ofy().load().type(AnTransaction::class.java).filter("an", an).order("date").list()
+    }
 
 }
--- src/main/java/demo/web/AdmissionNumberController.kt
+++ src/main/java/demo/web/AdmissionNumberController.kt
@@ -1,14 +1,25 @@
 package demo.web
 
+import demo.service.AnService
+import demo.service.AnTransactionService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @RequestMapping("an")
-class AdmissionNumberController{
+class AdmissionNumberController {
     @RequestMapping("{id}")
-    fun viewAn(@PathVariable id:Long, model: Model): String {
-       return "viewAn"
+    fun viewAn(@PathVariable id: Long, model: Model): String {
+        val admissionNumber = anService.findOne(id)
+        model.addAttribute("admissionNumber", admissionNumber)
+        if (admissionNumber != null) {
+            model.addAttribute("list", anTransactionService.findByAn(admissionNumber.an))
+        }
+        return "viewAn"
     }
+
+    @Autowired lateinit var anService: AnService
+    @Autowired lateinit var anTransactionService: AnTransactionService
 
 }
